e7552533785fee7b653c12cb8a60f558600ee8a8
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2021-03-27 20:45:43 +0200

    UnitTesting Added

---
+++ Code/PharmacyInformationSystem/src/unitTesting/Entry.java
@@ -0,0 +1,8 @@
+package unitTesting;
+
+public class Entry {
+	public static void main(String[] args) {
+		System.out.println("Let's begin some testing...");
+		//Call your other classes here to see your results
+	}
+}
