e0facf714c490e5ebbfcf45b0e7ee8e4b9998d39
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2021-03-30 19:51:25 +0300

    First UI commit

--- Code/PharmacyInformationSystem/src/application/Main.java
+++ Code/PharmacyInformationSystem/src/application/Main.java
@@ -4,18 +4,22 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage window) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			window.setTitle("Pharmacy Information System");
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("Login.fxml"));
+			Scene scene = new Scene(root,800,600);
+			window.setMinHeight(600);
+			window.setMinWidth(800);
+			root.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			window.setScene(scene);
+			window.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
