dde068f7fba4dd8fcf1279743cdb57c0ef1b9d72
Panagiotis Sklidas <panagiotis.skl@gmail.com>
2021-03-31 21:47:43 +0300

    minor chances

--- Code/Database/CREATE_PIS_DB.sql
+++ Code/Database/CREATE_PIS_DB.sql
@@ -1,4 +1,4 @@
 CREATE TABLE userRole (roleID NUMBER(2), roleDescription VARCHAR2(30), PRIMARY KEY (roleID));
-CREATE TABLE personnel (employeeID NUMBER(10) NOT NULL,firstName VARCHAR2(30), lastName VARCHAR2(30), idCard CHAR(8) UNIQUE, username CHAR(6) UNIQUE, passphrase VARCHAR2(40), 
+CREATE TABLE personnel (employeeID NUMBER(10) NOT NULL,firstName VARCHAR2(30), lastName VARCHAR2(30), idCard CHAR(8) UNIQUE, username CHAR(6) UNIQUE, passphrase CHAR(64), 
                         roleID NUMBER(2), PRIMARY KEY (employeeID), FOREIGN KEY (roleID) REFERENCES userRole(roleID));
 CREATE TABLE phone (employeeID NUMBER(10) NOT NULL, phoneNumber CHAR(10), PRIMARY KEY (phoneNumber, employeeID), FOREIGN KEY (employeeID) REFERENCES personnel(employeeID));
