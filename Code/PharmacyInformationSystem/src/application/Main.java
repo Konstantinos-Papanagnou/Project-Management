f550987b9560b3ce8b549f3ccda7b8968c1efc8c
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2021-04-02 18:42:51 +0300

    Redacted min height and width to constants

--- Code/PharmacyInformationSystem/src/application/Main.java
+++ Code/PharmacyInformationSystem/src/application/Main.java
@@ -1,18 +1,23 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import application.loginInterface.LoginScene;
+import application.loginInterface.LoginController;
 
 
 public class Main extends Application {
+	public static Stage window;
+	public static final double WIDTH = 1000;
+	public static final double HEIGHT = 800;
 	@Override
-	public void start(Stage window) {
+	public void start(Stage primaryStage) {
+		window = primaryStage;
 		window.setTitle("Pharmacy Information System");
-		window.setMinHeight(600);
-		window.setMinWidth(800);
-		LoginScene scene = new LoginScene();
-		window.setScene(scene.getLoginScene());
+		window.setMinHeight(HEIGHT);
+		window.setMinWidth(WIDTH);
+		window.setScene(new LoginController().getScene());
+		window.getIcons().add(new Image(getClass().getResourceAsStream("loginInterface/WelcomeImage.png")));
 		window.show();
 	}
 	
