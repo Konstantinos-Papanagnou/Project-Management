27e679f6950833ab57c3aaaacb68990b5a96b037
Panagiotis Sklidas <panagiotis.skl@gmail.com>
2021-04-04 18:51:14 +0300

    Maybe fixed the unit test failure
    1) if (first && !second){passed;}
    2) else {failed;}
    
    Explain 1)  if first is TRUE and second is NOT TRUE display passed
    So το πρόγραμμα θα εμφανίζει true μόνο αν οι δυο μεταβλητες είναι διαφορετικές.
    Αν διαγράψουμε το ! τότε λογικά δουλεύει σωστά για όλες της περιπτώσεις

--- Code/PharmacyInformationSystem/src/unitTesting/Entry.java
+++ Code/PharmacyInformationSystem/src/unitTesting/Entry.java
@@ -8,8 +8,8 @@ public class Entry {
 		System.out.println("Let's begin some testing...");
 		//Call your other classes here to see your results
 		boolean first = Sanitizer.checkIDCard("AM520145");
-		boolean second = Sanitizer.checkIDCard("AS526455");
-		if (first && !second)
+		boolean second = Sanitizer.checkIDCard("AM520145");
+		if (first && second)
 			System.out.println("PASSED");
 		else 
 			System.out.println("FAILED");
