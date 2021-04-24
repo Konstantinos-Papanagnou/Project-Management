b7e08a18eea65899e97bd0048b624e0fc5b47b31
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2021-04-24 22:44:30 +0300

    It works don't touch

--- Code/PharmacyInformationSystem/PharmacyInformationSystem/UIComponents/MainUserControls/StorageViewList/StorageListView.Designer.cs
+++ Code/PharmacyInformationSystem/PharmacyInformationSystem/UIComponents/MainUserControls/StorageViewList/StorageListView.Designer.cs
@@ -42,6 +42,7 @@ namespace PharmacyInformationSystem.UIComponents.MainUserControls.StorageViewLis
             this.Quality = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.DrugType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.listSelections = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ανανέωσηToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.EditMenu = new System.Windows.Forms.ToolStripMenuItem();
             this.RemoveMenu = new System.Windows.Forms.ToolStripMenuItem();
             this.storageNavigator1 = new PharmacyInformationSystem.UIComponents.MainUserControls.StorageViewList.StorageNavigator();
@@ -70,12 +71,13 @@ namespace PharmacyInformationSystem.UIComponents.MainUserControls.StorageViewLis
             this.List.FullRowSelect = true;
             this.List.HideSelection = false;
             this.List.Location = new System.Drawing.Point(0, 54);
-            this.List.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.List.Margin = new System.Windows.Forms.Padding(4);
             this.List.Name = "List";
             this.List.Size = new System.Drawing.Size(1259, 732);
             this.List.TabIndex = 0;
             this.List.UseCompatibleStateImageBehavior = false;
             this.List.View = System.Windows.Forms.View.Details;
+            this.List.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.List_ColumnClick);
             // 
             // DrugName
             // 
@@ -110,12 +112,12 @@ namespace PharmacyInformationSystem.UIComponents.MainUserControls.StorageViewLis
             // Cost
             // 
             this.Cost.Text = "Κόστος Αγοράς (€)";
-            this.Cost.Width = 167;
+            this.Cost.Width = 146;
             // 
             // Price
             // 
             this.Price.Text = "Τιμή Ποριόντος (€)";
-            this.Price.Width = 170;
+            this.Price.Width = 149;
             // 
             // Quality
             // 
@@ -125,32 +127,43 @@ namespace PharmacyInformationSystem.UIComponents.MainUserControls.StorageViewLis
             // DrugType
             // 
             this.DrugType.Text = "Είδος";
+            this.DrugType.Width = 130;
             // 
             // listSelections
             // 
             this.listSelections.BackColor = System.Drawing.SystemColors.ControlLight;
             this.listSelections.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.listSelections.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ανανέωσηToolStripMenuItem,
             this.EditMenu,
             this.RemoveMenu});
             this.listSelections.Name = "listSelections";
-            this.listSelections.Size = new System.Drawing.Size(215, 88);
+            this.listSelections.Size = new System.Drawing.Size(184, 88);
+            // 
+            // ανανέωσηToolStripMenuItem
+            // 
+            this.ανανέωσηToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ανανέωσηToolStripMenuItem.Name = "ανανέωσηToolStripMenuItem";
+            this.ανανέωσηToolStripMenuItem.Size = new System.Drawing.Size(183, 28);
+            this.ανανέωσηToolStripMenuItem.Text = "Ανανέωση";
+            this.ανανέωσηToolStripMenuItem.Click += new System.EventHandler(this.ανανέωσηToolStripMenuItem_Click);
             // 
             // EditMenu
             // 
             this.EditMenu.Font = new System.Drawing.Font("Calibri", 11F);
             this.EditMenu.Image = global::PharmacyInformationSystem.Properties.Resources.icons8_edit_row_24_1_1;
             this.EditMenu.Name = "EditMenu";
-            this.EditMenu.Size = new System.Drawing.Size(214, 28);
+            this.EditMenu.Size = new System.Drawing.Size(183, 28);
             this.EditMenu.Text = "Επεξεργασία";
             this.EditMenu.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.EditMenu.Click += new System.EventHandler(this.EditMenu_Click);
             // 
             // RemoveMenu
             // 
             this.RemoveMenu.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.RemoveMenu.Image = global::PharmacyInformationSystem.Properties.Resources.icons8_delete_bin_24;
             this.RemoveMenu.Name = "RemoveMenu";
-            this.RemoveMenu.Size = new System.Drawing.Size(214, 28);
+            this.RemoveMenu.Size = new System.Drawing.Size(183, 28);
             this.RemoveMenu.Text = "Διαγραφή";
             this.RemoveMenu.Click += new System.EventHandler(this.RemoveMenu_Click);
             // 
@@ -158,7 +171,7 @@ namespace PharmacyInformationSystem.UIComponents.MainUserControls.StorageViewLis
             // 
             this.storageNavigator1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
             this.storageNavigator1.Location = new System.Drawing.Point(0, 0);
-            this.storageNavigator1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.storageNavigator1.Margin = new System.Windows.Forms.Padding(5);
             this.storageNavigator1.Name = "storageNavigator1";
             this.storageNavigator1.Size = new System.Drawing.Size(1259, 55);
             this.storageNavigator1.TabIndex = 1;
@@ -170,9 +183,10 @@ namespace PharmacyInformationSystem.UIComponents.MainUserControls.StorageViewLis
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(43)))), ((int)(((byte)(48)))));
             this.Controls.Add(this.storageNavigator1);
             this.Controls.Add(this.List);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "StorageListView";
             this.Size = new System.Drawing.Size(1259, 786);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.StorageListView_Paint);
             this.listSelections.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -196,5 +210,6 @@ namespace PharmacyInformationSystem.UIComponents.MainUserControls.StorageViewLis
         private System.Windows.Forms.ToolStripMenuItem EditMenu;
         private System.Windows.Forms.ToolStripMenuItem RemoveMenu;
         private StorageNavigator storageNavigator1;
+        private System.Windows.Forms.ToolStripMenuItem ανανέωσηToolStripMenuItem;
     }
 }
--- Code/PharmacyInformationSystem/PharmacyInformationSystem/UIComponents/MainUserControls/StorageViewList/StorageListView.cs
+++ Code/PharmacyInformationSystem/PharmacyInformationSystem/UIComponents/MainUserControls/StorageViewList/StorageListView.cs
@@ -1,11 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using PharmacyInformationSystem.BusinessLogic;
 
@@ -14,18 +11,14 @@ namespace PharmacyInformationSystem.UIComponents.MainUserControls.StorageViewLis
     public partial class StorageListView : UserControl, IUpdatable<Medicine>
     {
         private readonly StoreKeeper User;
-        private List<Medicine> Medicines;
+        private bool FirstTime = true;
+        private readonly List<Medicine> Medicines;
+        private int sortColumn;
         public StorageListView(StoreKeeper User)
         {
             this.User = User;
             InitializeComponent();
-            //List.ItemMouseHover += (object sender, ListViewItemMouseHoverEventArgs e) => {
-            //    e.Item.BackColor = Color.Blue;
-            //};
-            var Medicines = User.GetMedicines();
-            foreach (var med in Medicines) {
-                AddToList(med);
-            }
+            Medicines = User.GetMedicines();
         }
 
         public void RefreshList()
@@ -40,17 +33,28 @@ namespace PharmacyInformationSystem.UIComponents.MainUserControls.StorageViewLis
 
         public void RefreshList(Medicine med, Operation op)
         {
-            User.AddMedicine(med);
-            AddToList(med);
-            Medicines.Add(med);
+            if (op == Operation.Add)
+            {
+                med = User.AddMedicine(med);
+                AddToList(med);
+                Medicines.Add(med);
+            }
+            else if(op == Operation.Update)
+            {
+                //Update
+                User.UpdateMedicine(med);
+                List.Items.RemoveAt(SearchforRemove(med.MedID, false));
+                AddToList(med);
+                Medicines.RemoveAt(SearchforRemove(med.MedID));
+                Medicines.Add(med);
+            }
         }
 
         private void AddToList(Medicine med)
         {
-            var lvi = new ListViewItem(med.MedName);
-
-            lvi.Tag = med.MedID;
-
+            var lvi = new ListViewItem(med.MedName){
+                Tag = med.MedID
+            };
             lvi.SubItems.Add(med.MedCategory);
             lvi.SubItems.Add(med.MedManfactureComp);
             lvi.SubItems.Add(med.MedDueDate);
@@ -79,28 +83,217 @@ namespace PharmacyInformationSystem.UIComponents.MainUserControls.StorageViewLis
         }
 
 
-        private int searchforRemove(int id)
+        private int SearchforRemove(int id, bool OurList = true)
         {
-            for (int i = 0; i < Medicines.Count;  i++)
-            {
-                if (Medicines[i].MedID == id)
-                {
-                    return i;
+            for (int i = 0; i < Medicines.Count; i++)
+                if (OurList) {
+                    if (Medicines[i].MedID == id)
+                        return i;
                 }
-            }
+                else
+                {
+                    if (int.Parse(List.Items[i].Tag.ToString()) == id)
+                        return i;
+                }
             return -1;
         }
 
+
         private void RemoveMenu_Click(object sender, EventArgs e)
         {
+            if (List.SelectedItems.Count <= 0) return;
             MedicineViewDelete medicineDelete = new MedicineViewDelete();
             if(medicineDelete.ShowDialog() == DialogResult.OK)
             {
-                 User.RemoveMedicine(int.Parse(List.SelectedItems[0].Tag.ToString()));
-                int index = searchforRemove(int.Parse(List.SelectedItems[0].Tag.ToString()));
+                User.RemoveMedicine(int.Parse(List.SelectedItems[0].Tag.ToString()));
+                int index = SearchforRemove(int.Parse(List.SelectedItems[0].Tag.ToString()));
                 Medicines.RemoveAt(index);
-                List.Items.RemoveAt(index);
+                List.Items.Remove(List.SelectedItems[0]);
+            }
+        }
+
+        private void EditMenu_Click(object sender, EventArgs e)
+        {
+            if (List.SelectedItems.Count <= 0) return;
+            ListViewItem lvi = List.SelectedItems[0];
+            Medicine temp = new Medicine(int.Parse(lvi.Tag.ToString()), lvi.Text, lvi.SubItems[1].Text, lvi.SubItems[2].Text, lvi.SubItems[3].Text, int.Parse(lvi.SubItems[4].Text), int.Parse(lvi.SubItems[5].Text),  double.Parse(lvi.SubItems[6].Text), double.Parse(lvi.SubItems[7].Text), lvi.SubItems[8].Text[0], lvi.SubItems[9].Text[0]);
+            new MedicineViewAdd(this, Operation.Update, temp).ShowDialog();
+        }
+
+
+        private void StorageListView_Paint(object sender, PaintEventArgs e)
+        {
+            if (!FirstTime) return;
+
+            FirstTime = false;
+            string notification = "";
+            foreach (var med in Medicines)
+            {
+                AddToList(med);
+                if (med.MedMinStock >= med.MedStockCount)
+                    notification += $"{med.MedName} πέρασε το κατώτερο δηλωμένο όριο {med.MedMinStock}\n";
+            }
+            if (string.IsNullOrEmpty(notification)) return;
+            MessageBox.Show(notification, "Ενημέρωση ανανέωσης", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        #region It ain't much but it's honest work
+        private void List_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            // Determine whether the column is the same as the last column clicked.
+            if (e.Column != sortColumn)
+            {
+                // Set the sort column to the new column.
+                sortColumn = e.Column;
+                // Set the sort order to ascending by default.
+                List.Sorting = SortOrder.Ascending;
+            }
+            else
+            {
+                // Determine what the last sort order was and change it.
+                if (List.Sorting == SortOrder.Ascending)
+                    List.Sorting = SortOrder.Descending;
+                else
+                    List.Sorting = SortOrder.Ascending;
+            }
+            List.SetSortIcon(sortColumn, List.Sorting);
+            // Call the sort method to manually sort.
+            List.Sort();
+            // Set the ListViewItemSorter property to a new ListViewItemComparer
+            // object.
+            this.List.ListViewItemSorter = new ListViewItemComparer(e.Column,
+                                                              List.Sorting);
+        }
+
+        public class ListViewItemComparer : IComparer
+        {
+
+            private int col;
+            private SortOrder order;
+            public ListViewItemComparer()
+            {
+                col = 0;
+                order = SortOrder.Ascending;
+            }
+            public ListViewItemComparer(int column, SortOrder order)
+            {
+                col = column;
+                this.order = order;
+            }
+            public int Compare(object x, object y)
+            {
+                int returnVal = -1;
+                returnVal = String.Compare(((ListViewItem)x).SubItems[col].Text,
+                                ((ListViewItem)y).SubItems[col].Text);
+                // Determine whether the sort order is descending.
+                if (order == SortOrder.Descending)
+                    // Invert the value returned by String.Compare.
+                    returnVal *= -1;
+                return returnVal;
+            }
+
+
+        }
+        #endregion
+
+        private void ανανέωσηToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RefreshList();
+        }
+    }
+    #region The Stack Overflow Region (Don't you dare touch me!)
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class ListViewExtensions
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HDITEM
+        {
+            public Mask mask;
+            public int cxy;
+            [MarshalAs(UnmanagedType.LPTStr)] public string pszText;
+            public IntPtr hbm;
+            public int cchTextMax;
+            public Format fmt;
+            public IntPtr lParam;
+            // _WIN32_IE >= 0x0300 
+            public int iImage;
+            public int iOrder;
+            // _WIN32_IE >= 0x0500
+            public uint type;
+            public IntPtr pvFilter;
+            // _WIN32_WINNT >= 0x0600
+            public uint state;
+
+            [Flags]
+            public enum Mask
+            {
+                Format = 0x4,       // HDI_FORMAT
+            };
+
+            [Flags]
+            public enum Format
+            {
+                SortDown = 0x200,   // HDF_SORTDOWN
+                SortUp = 0x400,     // HDF_SORTUP
+            };
+        };
+
+        public const int LVM_FIRST = 0x1000;
+        public const int LVM_GETHEADER = LVM_FIRST + 31;
+
+        public const int HDM_FIRST = 0x1200;
+        public const int HDM_GETITEM = HDM_FIRST + 11;
+        public const int HDM_SETITEM = HDM_FIRST + 12;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 msg, IntPtr wParam, ref HDITEM lParam);
+
+        public static void SetSortIcon(this ListView listViewControl, int columnIndex, SortOrder order)
+        {
+            IntPtr columnHeader = SendMessage(listViewControl.Handle, LVM_GETHEADER, IntPtr.Zero, IntPtr.Zero);
+            for (int columnNumber = 0; columnNumber <= listViewControl.Columns.Count - 1; columnNumber++)
+            {
+                var columnPtr = new IntPtr(columnNumber);
+                var item = new HDITEM
+                {
+                    mask = HDITEM.Mask.Format
+                };
+
+                if (SendMessage(columnHeader, HDM_GETITEM, columnPtr, ref item) == IntPtr.Zero)
+                {
+                    //It works - Don't Touch
+                    throw new Win32Exception();
+                }
+
+                if (order != SortOrder.None && columnNumber == columnIndex)
+                {
+                    switch (order)
+                    {
+                        case SortOrder.Ascending:
+                            item.fmt &= ~HDITEM.Format.SortDown;
+                            item.fmt |= HDITEM.Format.SortUp;
+                            break;
+                        case SortOrder.Descending:
+                            item.fmt &= ~HDITEM.Format.SortUp;
+                            item.fmt |= HDITEM.Format.SortDown;
+                            break;
+                    }
+                }
+                else
+                {
+                    item.fmt &= ~HDITEM.Format.SortDown & ~HDITEM.Format.SortUp;
+                }
+
+                if (SendMessage(columnHeader, HDM_SETITEM, columnPtr, ref item) == IntPtr.Zero)
+                {
+                    throw new Win32Exception();
+                }
             }
         }
     }
+    #endregion
 }
