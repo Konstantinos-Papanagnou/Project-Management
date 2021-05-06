b603ebd234f5e82b1d2219b54b65e3dab9816a38
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2021-05-06 14:25:09 +0300

    patched the script

--- Release/Current installer/setupDefaultDB.cmd
+++ Release/Current installer/setupDefaultDB.cmd
@@ -4,6 +4,7 @@ cls
 echo.
 echo.
 echo Setting up default database
+mkdir %localappdata%\PharmacyInformationSystem
 copy Pharmacy.db %localappdata%\PharmacyInformationSystem\
 
 echo Database was setted up successfully!
