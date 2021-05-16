0d073da9dcd9520d99665a26f0b6d5ee845319cf
Panagiotis Sklidas <panagiotis.skl@gmail.com>
2021-05-16 20:28:14 +0300

    Create Pharmacist.cs

---
+++ Code/PharmacyInformationSystem/PharmacyInformationSystem/BusinessLogic/Pharmacist.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyInformationSystem.BusinessLogic
+{
+    public class Pharmacist
+    {
+        public int PharmacistID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Phone { get; set; }
+        public string PANumber { get; set; }
+        public string PAStreet { get; set; }
+        public string PATown { get; set; }
+        public string PAPostalCode { get; set; }
+        public string PSellerID { get; set; }
+
+        public Pharmacist(int pharmacistID, string lastName, string firstName, string phone, string pANumber, string pAStreet, string pATown, string pAPostalCode, string pSellerID)
+        {
+            PharmacistID = pharmacistID;
+            LastName = lastName;
+            FirstName = firstName;
+            Phone = phone;
+            PANumber = pANumber;
+            PAStreet = pAStreet;
+            PATown = pATown;
+            PAPostalCode = pAPostalCode;
+            PSellerID = pSellerID;
+        }
+    }
+}
