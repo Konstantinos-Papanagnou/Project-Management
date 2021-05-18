e44c85005a293d2590f6e6a2f763776252606c42
Panagiotis Sklidas <panagiotis.skl@gmail.com>
2021-05-18 16:45:27 +0300

    Create OrderLine.cs

---
+++ Code/PharmacyInformationSystem/PharmacyInformationSystem/BusinessLogic/OrderLine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyInformationSystem
+{
+    class OrderLine
+    {
+        public int OrdID { get; set; }
+        public int MediID { get; set; }
+      /*  public string MediName { get; set; } */
+        public int ProductQuantity { get; set; }
+        public double TotalProductCost { get; set; }
+
+        public OrderLine(int ordID, int mediID, int productQuantity, double totalProductCost)
+        {
+            OrdID = ordID;
+            MediID = mediID;
+            ProductQuantity = productQuantity;
+            TotalProductCost = totalProductCost;
+        }
+    }
+}
