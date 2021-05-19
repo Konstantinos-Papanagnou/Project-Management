e41fdd4d858de63ca4324fb5698825e4b33bf93d
Panagiotis Sklidas <panagiotis.skl@gmail.com>
2021-05-19 12:06:48 +0300

    Minor change in order.cs

--- Code/PharmacyInformationSystem/PharmacyInformationSystem/BusinessLogic/Order.cs
+++ Code/PharmacyInformationSystem/PharmacyInformationSystem/BusinessLogic/Order.cs
@@ -9,9 +9,6 @@ namespace PharmacyInformationSystem
     class Order
     {
         public int OrderID { get; set; }
-        public double TotalCost { get; set; }
-        public string OrderDate { get; set; }
-        public List<string> OrderList { get; set; }
         public int SellerIDOrder { get; set; }
         public string SellerFirstName { get; set; }
         public string SellerLaststName { get; set; }
@@ -23,13 +20,13 @@ namespace PharmacyInformationSystem
         public string PharmaAddressTown { get; set; }
         public string PharmaAddressPostalCode { get; set; }
         public string PharmaPhoneNumber { get; set; }
+        public double TotalCost { get; set; }
+        public string OrderDate { get; set; }
+        public List<string> OrderList { get; set; }
 
-        public Order(int orderID, double totalCost, string orderDate, List<string> orderList, int sellerIDOrder, string sellerFirstName, string sellerLaststName, int pharmacistIDOrder, string pharmaFirstName, string pharmaLaststName, string pharmaAddressNumber, string pharmaAddressStreet, string pharmaAddressTown, string pharmaAddressPostalCode, string pharmaPhoneNumber)
+        public Order(int orderID, int sellerIDOrder, string sellerFirstName, string sellerLaststName, int pharmacistIDOrder, string pharmaFirstName, string pharmaLaststName, string pharmaAddressNumber, string pharmaAddressStreet, string pharmaAddressTown, string pharmaAddressPostalCode, string pharmaPhoneNumber, double totalCost, string orderDate, List<string> orderList)
         {
             OrderID = orderID;
-            TotalCost = totalCost;
-            OrderDate = orderDate;
-            OrderList = orderList;
             SellerIDOrder = sellerIDOrder;
             SellerFirstName = sellerFirstName;
             SellerLaststName = sellerLaststName;
@@ -41,6 +38,9 @@ namespace PharmacyInformationSystem
             PharmaAddressTown = pharmaAddressTown;
             PharmaAddressPostalCode = pharmaAddressPostalCode;
             PharmaPhoneNumber = pharmaPhoneNumber;
+            TotalCost = totalCost;
+            OrderDate = orderDate;
+            OrderList = orderList;
         }
     }
 }
