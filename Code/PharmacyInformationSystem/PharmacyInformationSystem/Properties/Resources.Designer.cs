0a3c90a1e7b1d8aacd64057c5d0c27604eedd8bb
elgivann <elgivann@users.noreply.github.com>
2021-05-29 22:34:25 +0300

    order ui

--- Code/PharmacyInformationSystem/PharmacyInformationSystem/Properties/Resources.Designer.cs
+++ Code/PharmacyInformationSystem/PharmacyInformationSystem/Properties/Resources.Designer.cs
@@ -353,6 +353,16 @@ namespace PharmacyInformationSystem.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icons8_invoice_26 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-invoice-26", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_pill_bottle_24_1_1 {
             get {
                 object obj = ResourceManager.GetObject("icons8-pill-bottle-24(1)1", resourceCulture);
